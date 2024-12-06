3ff391fe0532976f3fa7f49e0a68bcb3a865066a
Sarangthem Amarjit Meetei <sarangthemamarjit123@gmail.com>
2024-12-06 08:59:11 +0530

    updated logiinpage

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,7 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isStayed = false;
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  // Example: Hardcoded correct username and password
+  final String correctUsername = "admin";
+  final String correctPassword = "12345";
+
+  void _validateAndLogin() {
+    String username = _usernameController.text;
+    String password = _passwordController.text;
+
+    if (username.isEmpty || password.isEmpty) {
+      _showDialog("Error", "Username or Password cannot be empty.");
+    } else if (username != correctUsername || password != correctPassword) {
+      _showDialog("Login Failed", "Incorrect Username or Password.");
+    } else {
+      _showDialog("Login Success", "Welcome, $username!");
+    }
+  }
+
+  void _showDialog(String title, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,134 +58,184 @@ class LoginPage extends StatelessWidget {
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/images/bg.png'), fit: BoxFit.cover)),
-        child: Center(
-          child: Card(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-            elevation: 10,
-            child: Container(
-              width: 350,
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: const Color(0xFF2C2C2C),
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.5),
-                    blurRadius: 15,
-                    offset: const Offset(0, 5),
-                  ),
-                ],
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset(
-                    'assets/images/logo.png',
-                    height: 50,
-                  ),
-                  // Spotify Logo
-                  // Row(
-                  //   children: [
-                  //     Image.asset(
-                  //       'assets/images/logo.png',
-                  //       height: 30,
-                  //     ),
-                  //     Text(
-                  //       'ILP MANIPUR',
-                  //       style: GoogleFonts.kulimPark(
-                  //         letterSpacing: 5,
-                  //         color: Colors.white,
-                  //         fontSize: 24,
-                  //         fontWeight: FontWeight.bold,
-                  //       ),
-                  //     ),
-                  //   ],
-                  // ),
-                  const SizedBox(height: 20),
-                  // Tabs for Sign In and Sign Up
-                  const Text(
-                    'LOG IN',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  // Username Field
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Username',
-                      hintStyle: const TextStyle(color: Colors.white70),
-                      filled: true,
-                      fillColor: const Color(0xFF3A3A3A),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide.none,
-                      ),
-                    ),
-                    style: const TextStyle(color: Colors.white),
-                  ),
-                  const SizedBox(height: 15),
-                  // Password Field
-                  TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      hintStyle: const TextStyle(color: Colors.white70),
-                      filled: true,
-                      fillColor: const Color(0xFF3A3A3A),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide.none,
-                      ),
-                    ),
-                    style: const TextStyle(color: Colors.white),
-                  ),
-                  const SizedBox(height: 15),
-                  // Stay Signed In
-                  Row(
-                    children: [
-                      Checkbox(
-                        value: true,
-                        onChanged: (value) {},
-                        activeColor: Colors.green,
-                      ),
-                      const Text(
-                        'Stay signed in',
-                        style: TextStyle(color: Colors.white70),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  // Sign In Button
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      minimumSize: const Size(double.infinity, 50),
-                    ),
-                    child: const Text(
-                      'LOG IN',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Colors.white),
-                    ),
-                  ),
-                  const SizedBox(height: 30),
-                  // Forgot Password
-                ],
-              ),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: const AssetImage('assets/images/bg4.png'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.4), // Adjust opacity as needed
+              BlendMode.darken, // Blend mode to apply black overlay
             ),
           ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // "Welcome to Manipur"
+            Text(
+              'Welcome to Manipur',
+              style: GoogleFonts.greatVibes(
+                fontSize: 45, // Adjust as needed
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 10),
+            // "The Jewelled Land"
+            Text(
+              '- THE JEWELLED LAND -',
+              style: GoogleFonts.merriweather(
+                letterSpacing: 3,
+                foreground: Paint()
+                  ..style = PaintingStyle.stroke
+                  ..strokeWidth = 1
+                  ..color = Colors.orange, // Border color
+                fontSize: 20, // Adjust as needed
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 50),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Card(
+                  color: Colors.transparent,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  elevation: 10,
+                  child: Container(
+                    width: 350,
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          color: const Color.fromARGB(255, 192, 191, 191)),
+                      color: const Color(0xFF2C2C2C).withOpacity(0.5),
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.3),
+                          blurRadius: 10,
+                          offset: const Offset(0, 5),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.asset(
+                          'assets/images/logo.png',
+                          height: 50,
+                        ),
+                        const SizedBox(height: 20),
+                         Column(
+                          children: [
+                            Text(
+                              'Welcome!',
+                              style: GoogleFonts.kulimPark(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25,
+                              ),
+                            ),
+                            Text(
+                              'Log in to your Account',
+                              style: GoogleFonts.kulimPark(
+                                color: Colors.white54,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        // Username Field
+                        TextField(
+                          controller: _usernameController,
+                          decoration: InputDecoration(
+                            hintText: 'Username',
+                            hintStyle: const TextStyle(
+                              color: Color.fromARGB(179, 128, 127, 127),
+                            ),
+                            filled: true,
+                            fillColor: const Color(0xFF3A3A3A),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            prefixIcon: const Icon(
+                              size: 20,
+                              FontAwesomeIcons.user, // Icon for username
+                              color: Color.fromARGB(255, 222, 228, 211),
+                            ),
+                          ),
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        const SizedBox(height: 15),
+                        // Password Field
+                        TextField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            hintText: 'Password',
+                            hintStyle: const TextStyle(
+                              color: Color.fromARGB(179, 128, 127, 127),
+                            ),
+                            filled: true,
+                            fillColor: const Color(0xFF3A3A3A),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            prefixIcon: const Icon(
+                              FontAwesomeIcons.lock, // Icon for password
+                              color: Color.fromARGB(255, 222, 228, 211),
+                              size: 20,
+                            ),
+                          ),
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        const SizedBox(height: 15),
+                        Row(
+                          children: [
+                            Checkbox(
+                              value: isStayed,
+                              onChanged: (value) {
+                                setState(() {
+                                  isStayed = value!;
+                                });
+                              },
+                              activeColor: Colors.green,
+                            ),
+                            const Text(
+                              'Stay signed in',
+                              style: TextStyle(color: Colors.white70),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        // Sign In Button
+                        ElevatedButton(
+                          onPressed: _validateAndLogin,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            minimumSize: const Size(double.infinity, 50),
+                          ),
+                          child: const Text(
+                            'LOG IN',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.white),
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
